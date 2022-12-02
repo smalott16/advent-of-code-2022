34ccf3c3c4da63432a75996b9578de131c9f2bc8
Spencer Malott <smalott@acromedia.com>
2022-12-02 14:48:49 -0800

    part 2 solution

--- day-02/solution.mjs
+++ day-02/solution.mjs
@@ -2,7 +2,8 @@ import { readFile } from 'node:fs/promises'
 
 /**
  * Challenger A = Rock, B = Paper, C = Scissors
- * Response X = Rock, Y = Paper, Z = Scissors 
+ * Response Part 1 X = Rock, Y = Paper, Z = Scissors 
+ * Response Part 2 X = loss, Y = tie, Z = win
  */
 async function getDataAsArray () {
   try {
@@ -13,12 +14,12 @@ async function getDataAsArray () {
   }
 }
 
-function getScore (gameArray) {
+function getScorePart1 (gameArray) {
   const shapeSummary = {
     'X': {
       score: 1,
       beats: 'C',
-      ties: 'A'
+      ties: 'A',
     },
     'Y': {
       score: 2,
@@ -48,8 +49,47 @@ function getScore (gameArray) {
   
 }
 
+function getScorePart2 (gameArray) {
+  // Key determines if we lose, tie or win.
+  const scoreKey = {
+    'X': {
+      'A': 3,  // If X loses to A (Rock) play Scisors (3 points)
+      'B': 1,  // Rock
+      'C': 2
+    },
+    'Y': {
+      'A': 1, // If Y ties A (Rock), play rock (1 point)
+      'B': 2,
+      'C': 3
+    },
+    'Z': {
+      'A': 2, // If Z beats A (Rock), play paper (2 points)
+      'B': 3,
+      'C': 1
+    }
+  }
+
+  const opponent = gameArray[0]
+  const result = gameArray[1]
+  const baseScore = scoreKey[result][`${opponent}`]
+  // is win?
+  if (result === 'Z') {
+    return 6 + baseScore
+  }
+  // is tie?
+  if (result === 'Y') {
+    return 3 + baseScore
+  }
+  // isnt a win or a tie
+  return 0 + baseScore  
+}
+
 const rpsGames = await getDataAsArray()
-const finalScore = rpsGames.reduce((total, currentValue) => {
-  return total + getScore(currentValue)
+const finalScorePart1 = rpsGames.reduce((total, currentValue) => {
+  return total + getScorePart1(currentValue)
 }, 0)
-console.log(finalScore)
+const finalScorePart2 = rpsGames.reduce((total, currentValue) => {
+  return total + getScorePart2(currentValue)
+}, 0)
+console.log('Part 1 final score:', finalScorePart1)
+console.log('Part 2 final score:', finalScorePart2)
