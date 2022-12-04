8efcd4b9d6e457add9aca75d1eecab91e0b3fbbf
Spencer Malott <spencermalott16@gmail.com>
2022-12-04 08:58:57 -0800

    solution part 1

---
+++ day-04/solution.mjs
@@ -0,0 +1,19 @@
+import fs from 'fs'
+
+const assignments = (fs.readFileSync('assignments.txt', 'utf8')).split('\n')
+let pairsPart1 = 0
+
+assignments.forEach((assignment, index) => {
+  const schedulePair = assignment.split(',')
+  const schedule1 = schedulePair[0].split('-').map(value => parseInt(value))
+  const schedule2 = schedulePair[1].split('-').map(value => parseInt(value))
+  
+  // does 1 fit into 2 or 2 fit into 1?
+  if ((schedule1[0] <= schedule2[0] && schedule1[1] >= schedule2[1])
+    || (schedule2[0] <= schedule1[0] && schedule2[1] >= schedule1[1])) {
+      pairsPart1 +=1
+  }
+})
+
+console.log('The number of fully overlapping assignments is:', pairsPart1)
+
