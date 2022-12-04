32701f5c365123bb0c2019104fa363ac6a034b78
Spencer Malott <spencermalott16@gmail.com>
2022-12-04 09:22:05 -0800

    solution part 2

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - Day 1: Calorie counting (JS)
 - Day 2: Rock Paper Scissors (JS)
 - Day 3: Rucksack Reorganization (JS)
+- Day 4: Camp Cleanup (JS)
--- day-04/solution.mjs
+++ day-04/solution.mjs
@@ -2,6 +2,7 @@ import fs from 'fs'
 
 const assignments = (fs.readFileSync('assignments.txt', 'utf8')).split('\n')
 let pairsPart1 = 0
+let pairsPart2 = 0
 
 assignments.forEach((assignment, index) => {
   const schedulePair = assignment.split(',')
@@ -12,8 +13,17 @@ assignments.forEach((assignment, index) => {
   if ((schedule1[0] <= schedule2[0] && schedule1[1] >= schedule2[1])
     || (schedule2[0] <= schedule1[0] && schedule2[1] >= schedule1[1])) {
       pairsPart1 +=1
+      pairsPart2 +=1
+      return
+  }
+
+  // if they aren't fully overlapping, do they overlap at all?
+  if ((schedule1[0] <= schedule2[0] && (schedule1[1] >= schedule2[0]))
+    || schedule2[0] <= schedule1[0] && schedule2[1] >= schedule1[0]) {
+      pairsPart2 +=1
   }
 })
 
 console.log('The number of fully overlapping assignments is:', pairsPart1)
+console.log('The number assignments with any overlap at all is:', pairsPart2)
 
