43c572e7199c6cc0c6b22edc0deeea1701cfeece
Spencer Malott <smalott@acromedia.com>
2022-12-07 16:38:48 -0800

    day 7 solution

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 - Day 4: Camp Cleanup (JS)
 - Day 5: Supply Stacks (JS)
 - Day 6: Tuning Trouble (JS)
+- Day 7: No Space Left On Device (JS)
--- day-07/solution.mjs
+++ day-07/solution.mjs
@@ -1,11 +1,16 @@
+import { Console } from 'console'
 import fs from 'fs'
 
-const commandLog = (fs.readFileSync('test.txt', 'utf-8')).split('\n')
+const commandLog = (fs.readFileSync('commands.txt', 'utf-8')).split('\n')
+
+// give a unique identifier to all directories
+
 class Directory {
   constructor (name) {
     this.name = name
     this.files = []
     this.directories = []
+    this.fileSize = undefined
     this.directorySize = undefined
   }
   addFile({ name, size }) {
@@ -14,16 +19,24 @@ class Directory {
   addDirectory(directory) {
     this.directories.push(directory)
   }
+  calculateFileSize() {
+    const files = this.files.reduce((total, currentVal) => {
+      return total + currentVal.size
+    },0)
+    this.fileSize = files
+  }
 }
 
 // Build Directory Strucutre
 let history = []
 const directories = {}
-commandLog.forEach((cmd) => {
+commandLog.forEach((cmd, index) => {
+  
   if (cmd.includes('$ cd') && !cmd.includes('..')) {
     const dirName = cmd.split(' ').pop()
-    history.push(dirName)
-    directories[dirName] = new Directory(dirName)
+    const uniqueDirName = `${dirName}${history.join('')}`
+    history.push(uniqueDirName)
+    directories[uniqueDirName] = new Directory(uniqueDirName)
   }
   if (cmd === '$ cd ..') {
     history.pop()
@@ -34,7 +47,7 @@ commandLog.forEach((cmd) => {
   if (cmd.includes('dir')) {
     const dirNameAdd = cmd.split(' ').pop()
     //directories[currentDir].addDirectory(new Directory(dirNameAdd))
-    directories[currentDir].addDirectory(dirNameAdd)
+    directories[currentDir].addDirectory(`${dirNameAdd}${history.join('')}`)
   }
   // if cmd includes a number then add it to files
   if (!cmd.includes('dir') && !cmd.includes('$')) {
@@ -48,6 +61,48 @@ commandLog.forEach((cmd) => {
   }
 })
 
-console.log(directories['e'].directories)
+const directoryNames = Object.keys(directories)
+// Update directory file size property
+directoryNames.forEach((key) => {
+  directories[key].calculateFileSize()
+})
 
+// Recursion function to calculate directory size
+function deeplyCalculateDirectorySize (directory) {
+  let total = directory.fileSize
 
+  if (directory.directories.length > 0) {
+    directory.directories.forEach((subDir) => {
+      const subDirFileSize = deeplyCalculateDirectorySize(directories[subDir])
+      total += subDirFileSize
+    })
+  }
+  
+  return total
+}
+
+// Calculate cumulative of directories containing 100k or less
+let cumulativeSize = 0
+directoryNames.forEach((subDir) => {
+  const size = deeplyCalculateDirectorySize(directories[subDir])
+  directories[subDir].directorySize = size
+  if (size <= 100000) {
+    cumulativeSize += size
+  } 
+})
+console.log(cumulativeSize)
+
+const remainingMemory = 70000000 - directories['/'].directorySize
+const requiredFreeSpace = 30000000 - remainingMemory
+console.log('Remaining Memory:', remainingMemory, 'Required Free Space:', requiredFreeSpace)
+
+let smallestViableDirectory = 70000000
+directoryNames.forEach((subDir) => {
+  if (directories[subDir].directorySize >= requiredFreeSpace
+    && directories[subDir].directorySize < smallestViableDirectory) 
+  {
+    smallestViableDirectory=directories[subDir].directorySize
+  }
+      
+})
+console.log(smallestViableDirectory)
